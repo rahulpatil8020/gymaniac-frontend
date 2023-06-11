496a99d643c95d0a4979704a22d77a30f4ab7853
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-06-11 12:45:15 -0700

    Created file structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "pages/HomePage";
+import AuthPage from "pages/AuthPage";
+import ProfilePage from "pages/ProfilePage";
+
 function App() {
   return <div className="App">Hello World</div>;
 }
