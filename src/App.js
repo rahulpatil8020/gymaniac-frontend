4ba19a6817686a2631182ae336acfbde32a809c7
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-06-12 12:43:37 -0700

    added redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "pages/HomePage";
 import AuthPage from "pages/AuthPage";
 import ProfilePage from "pages/ProfilePage";
+import ChatPage from "pages/ChatPage";
+import PrivateRoutes from "utils/PrivateRoutes";
+// import Navbar from "components/Navbar";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/utils/PrivateRoutes.js
@@ -0,0 +1,19 @@
+import { Outlet, Navigate } from "react-router-dom";
+import React, { useState } from "react";
+import Navbar from "components/Navbar";
+
+const PrivateRoutes = ({ children, ...rest }) => {
+  // const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const user = true;
+
+  return user ? (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to="/auth" />
+  );
+};
+
+export default PrivateRoutes;
