86975f3a936bea5cdb3f3a682b06137b0d615092
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-06-15 22:42:41 -0700

    Checking if the data is being saved on local storage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const HomePage = () => {
-  const user = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.auth);
   React.useEffect(() => {
     console.log(user);
-  }, []);
+  }, [user]);
+  console.log(user);
   return <div>HomePage</div>;
 };
 
