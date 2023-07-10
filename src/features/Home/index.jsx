0e17e7159add5b925f4e1283fc0db9d83c6f343f
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-07-10 15:43:51 -0700

    Added the widgets on home screen

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,13 +1,68 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import HomeSkeleton from "./HomeSkeleton";
+import { Skeleton, Grid, Box, Container, Stack } from "@mui/material";
+import UserWidget from "./UserWidget";
+import GoalsWidget from "./GoalsWidget";
+import WidgetWrapper from "components/WidgetWrapper";
+import AddPostWidget from "./AddPostWidget";
+import PostWidget from "./PostWidget";
 
 const HomePage = () => {
   const { user } = useSelector((state) => state.auth);
   return (
-    <div>
-      <HomeSkeleton />
-    </div>
+    <Container maxWidth="lg">
+      <Grid sx={{ paddingTop: 3 }} container spacing={3}>
+        <Grid height={"100%"} item xs={12} md={3}>
+          <Box
+            sx={{
+              // maxHeight: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "left",
+              justifyContent: "center",
+            }}
+          >
+            <UserWidget />
+            <Box
+              sx={{
+                marginTop: 5,
+                height: "20rem",
+                overflow: "hidden",
+              }}
+            >
+              <GoalsWidget />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <AddPostWidget />
+          <PostWidget />
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "left",
+              justifyContent: "center",
+            }}
+          >
+            <Box>
+              <Skeleton variant="rounded" height="25vh" />
+            </Box>
+            <Box
+              sx={{
+                marginTop: 5,
+                height: "20rem",
+                overflow: "hidden",
+              }}
+            >
+              <Skeleton variant="rounded" height="40vh" />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
