6f823104c14678e0ca20b94a83209b3772b4b281
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-07-13 21:33:30 -0700

    Goals widget is completed with full functionality

--- src/components/AvatarAndName.jsx
+++ src/components/AvatarAndName.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Avatar, Typography, useTheme, Stack } from "@mui/material";
+import { Box, Avatar, Typography, useTheme, Stack, Link } from "@mui/material";
 import avatarImage from "../assets/1.jpg";
 
 const AvatarAndName = () => {
@@ -15,20 +15,25 @@ const AvatarAndName = () => {
           marginRight: 1.5,
         }}
       />
-
-      <Stack>
-        <Typography
-          color={dark}
-          sx={{ paddingTop: 0.5 }}
-          lineHeight={0.8}
-          variant="h6"
-        >
-          Rahul Patil
-        </Typography>
-        <Typography color={mediumMain} variant="body1">
-          rahulpatil20
-        </Typography>
-      </Stack>
+      <Link
+        sx={{ textDecoration: "none" }}
+        component={"button"}
+        onClick={() => console.log("user")}
+      >
+        <Stack>
+          <Typography
+            color={dark}
+            sx={{ paddingTop: 0.5 }}
+            lineHeight={0.8}
+            variant="h6"
+          >
+            Rahul Patil
+          </Typography>
+          <Typography color={mediumMain} variant="body1">
+            rahulpatil20
+          </Typography>
+        </Stack>
+      </Link>
     </Box>
   );
 };
--- src/features/Home/GoalsWidget.jsx
+++ src/features/Home/GoalsWidget.jsx
@@ -3,45 +3,199 @@ import {
   Typography,
   useTheme,
   Divider,
-  Button,
   Checkbox,
+  InputBase,
+  IconButton,
+  Alert,
+  Snackbar,
+  Button,
 } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 import React, { useState } from "react";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-
+import AddIcon from "@mui/icons-material/Add";
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
 const GoalsWidget = () => {
   const theme = useTheme();
-  const [goals, setGoals] = useState([1, 3, 4, 5]);
+
+  const [goals, setGoals] = useState([
+    { id: 1, title: "New Goal 1", completed: false },
+    { id: 2, title: "New Goal 2", completed: false },
+    { id: 3, title: "New Goal 3", completed: true },
+  ]);
+  const [goal, setGoal] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [goalAlreadyExists, setGoalAlreadyExists] = useState(false);
+
+  const neutralLight = theme.palette.neutral.light;
+  const handleCreateGoal = () => {
+    let temp = true;
+    if (!goal) return;
+    goals.map((item) => {
+      if (
+        compareStringsIgnoreExtra(item?.title, goal?.title) &&
+        !item.completed
+      ) {
+        setGoal(null);
+        setGoalAlreadyExists(true);
+        temp = false;
+      }
+    });
+
+    if (temp) {
+      const newGoal = { ...goal, completed: false, id: goals.length + 1 };
+      setGoals((prev) => [...prev, newGoal]);
+      setGoal(null);
+    }
+  };
+
+  const compareStringsIgnoreExtra = (str1, str2) => {
+    const cleanStr1 = str1.replace(/[^\w\s]/g, "").toLowerCase();
+    const cleanStr2 = str2.replace(/[^\w\s]/g, "").toLowerCase();
+    return cleanStr1 === cleanStr2;
+  };
+
+  const handleDeleteGoal = (ele) => {
+    setGoals((prev) => prev.filter((goal) => goal.id !== ele.id));
+  };
+
+  const handleEditing = (goal) => {
+    setIsEditing(true);
+    setGoal(goal);
+  };
+
+  const handleCancleEditing = () => {
+    setIsEditing(false);
+    setGoal(null);
+  };
+
+  const handleEditGoal = () => {
+    let temp = true;
+    if (!goal) return;
+    goals.map((item) => {
+      if (
+        compareStringsIgnoreExtra(item?.title, goal?.title) &&
+        !item.completed
+      ) {
+        // setGoal(null);
+        setGoalAlreadyExists(true);
+        temp = false;
+      }
+    });
+    if (temp) {
+      setGoals((prev) =>
+        prev.map((item) => {
+          if (item.id === goal.id) {
+            item = { ...item, title: goal.title };
+            return item;
+          }
+          return item;
+        })
+      );
+      setIsEditing(false);
+      setGoal(null);
+    }
+    return;
+  };
+
+  const handleGoalComplete = (ele) => {
+    setGoals((prev) =>
+      prev.map((item) => {
+        if (item.id === ele.id) {
+          item = { ...item, completed: true };
+          return item;
+        }
+        return item;
+      })
+    );
+  };
+
+  const createSnackbar = () => (
+    <Snackbar
+      onClose={() => {
+        setGoalAlreadyExists(false);
+      }}
+      open={goalAlreadyExists}
+      autoHideDuration={3000}
+    >
+      <Alert severity="warning" sx={{ width: "100%" }}>
+        This Goal Already Exists
+      </Alert>
+    </Snackbar>
+  );
+
   return (
-    <WidgetWrapper>
-      <Stack
-        divider={<Divider orientation="horizontal" />}
-        direction={"column"}
-        spacing={1}
-      >
-        <Typography alignSelf={"center"} variant="h6">
-          Workout Goals
-        </Typography>
-        <Stack>
-          {goals.map((i) => (
-            <FlexBetween>
-              <Stack direction={"row"} alignItems="center">
-                <Checkbox />
-                <Typography>New Goal</Typography>
-              </Stack>
-              <Stack direction="row">
-                <EditOutlinedIcon />
-                <DeleteOutlineIcon />
-              </Stack>
+    <>
+      {createSnackbar()}
+      <WidgetWrapper>
+        <Stack
+          divider={<Divider orientation="horizontal" />}
+          direction={"column"}
+          spacing={1}
+        >
+          <Typography alignSelf={"center"} variant="h6">
+            Workout Goals
+          </Typography>
+          <Stack>
+            {goals.map((ele, i) => {
+              if (ele.completed) return <></>;
+              return (
+                <>
+                  <FlexBetween>
+                    <Stack direction={"row"} alignItems="center">
+                      <Checkbox onChange={() => handleGoalComplete(ele)} />
+                      <Typography width={125} noWrap textOverflow={"ellipsis"}>
+                        {ele.title}
+                      </Typography>
+                    </Stack>
+                    <Stack direction="row">
+                      {isEditing && ele.id === goal.id ? (
+                        <IconButton onClick={handleCancleEditing}>
+                          <CloseIcon />
+                        </IconButton>
+                      ) : (
+                        <IconButton onClick={() => handleEditing(ele)}>
+                          <EditOutlinedIcon />
+                        </IconButton>
+                      )}
+                      <IconButton onClick={() => handleDeleteGoal(ele)}>
+                        <DeleteOutlineIcon />
+                      </IconButton>
+                    </Stack>
+                  </FlexBetween>
+                </>
+              );
+            })}
+          </Stack>
+          <Stack spacing={1}>
+            <FlexBetween
+              backgroundColor={neutralLight}
+              borderRadius="9px"
+              padding="0.1rem 1rem"
+            >
+              <InputBase
+                value={goal ? goal.title : ""}
+                onChange={(e) => setGoal({ ...goal, title: e.target.value })}
+                placeholder="Create a Goal..."
+              />
+              {isEditing ? (
+                <IconButton onClick={handleEditGoal}>
+                  <DoneIcon />
+                </IconButton>
+              ) : (
+                <IconButton onClick={handleCreateGoal}>
+                  <AddIcon />
+                </IconButton>
+              )}
             </FlexBetween>
-          ))}
+          </Stack>
         </Stack>
-        <Button> Create a Goal</Button>
-      </Stack>
-    </WidgetWrapper>
+        <Button sx={{ marginTop: 1 }}>View All</Button>
+      </WidgetWrapper>
+    </>
   );
 };
 
