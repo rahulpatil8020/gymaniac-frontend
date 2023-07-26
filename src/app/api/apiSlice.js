bfcc26fe91d9e2acd5fc6b13f1cf1bed41471359
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-07-26 09:22:14 -0700

    using prepare body

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -12,6 +12,15 @@ const baseQuery = fetchBaseQuery({
     }
     return headers;
   },
+  prepareBody: (body) => {
+    console.log(body);
+    const { media, otherData } = body;
+    const formData = new FormData();
+    formData.append("media", media);
+    formData.append("otherData", JSON.stringify(otherData));
+    console.log(formData, "INSIDE PREPARING");
+    return formData;
+  },
 });
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
