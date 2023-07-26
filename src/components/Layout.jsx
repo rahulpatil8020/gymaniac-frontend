d6c92a2f2847d19f92510d507d823a0df9a51925
rahulpatil8020 <rahuldpatil20@gmail.com>
2023-07-26 09:21:04 -0700

    getting user data while app starting

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-// import { useGetUserInfoQuery } from "features/User/userApiSlice";
+import { useGetUserInfoQuery } from "features/User/userApiSlice";
 
 const Layout = () => {
-  // const { data: user } = useGetUserInfoQuery();
+  const { data: user } = useGetUserInfoQuery();
   return (
     <>
       <Navbar />
